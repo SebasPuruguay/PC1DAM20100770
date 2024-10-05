17d7152340239bc66ed419ad2292aaac08d08c5b
SebasPuruguay <20100770@ue.edu.pe>
2024-10-05 09:46:34 -0500

    parte 1

--- app/src/main/java/com/example/pc1dam20100770/LoginActivity.kt
+++ app/src/main/java/com/example/pc1dam20100770/LoginActivity.kt
@@ -1,20 +1,40 @@
 package com.example.pc1dam20100770
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        emailEditText = findViewById(R.id.edTxEmail)
+        passwordEditText = findViewById(R.id.edTxtPass)
+        loginButton = findViewById(R.id.btnLog)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email == "invitado@android.com" && password == "invitado123") {
+                // Redirigir al Navigation Drawer
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Opcional: finalizar LoginActivity
+            } else {
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
